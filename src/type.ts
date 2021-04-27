26838f44bf0d18f702874d879d25812c48f07178
scwon <a49297466@gmail.com>
2021-04-27 14:19:04 +0900

    설명 보강

--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,12 @@
-export type Category = "none" | "node" | "image" | "pod" | "rs";
+export type Category = "none" | "node" | "image" | "pod" | "rs"; // 해당 값으로 분류
 
 export interface DataType {
-	oname: string // 해당 컨테이너의 이름
+	name: string // 해당 컨테이너의 alias 이름
+	container: number // primary key
 	onodeName: string // category node
 	imageName: string // category image
 	podName: string // category pod
-	replicasetName: string | null // category rs // null의 경우 모으지 않고 각각 표시
+	replicasetName: string | null // category rs // null 의 경우 모으지 않고 각각 표시
 	cpu_total: number // CPU 사용량
-	[key: string]: any
+	[key: string]: any // 그 외 데이터는 무시하면 됩니다.
 }
